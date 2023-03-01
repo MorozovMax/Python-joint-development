797d8771d8b30cc54a73af0ea23c828a13d5ef6b
Max <mrzvmax@gmail.com>
2023-03-01 19:05:03 +0300

    Create game logic

--- 03_MergeRequirements/bullscows.py
+++ 03_MergeRequirements/bullscows.py
@@ -1,3 +1,7 @@
+import random
+import sys
+import urllib.request
+
 def bullscows(guess: str, secret: str) -> (int, int):
     bulls = 0
     cows = 0
@@ -8,6 +12,34 @@ def bullscows(guess: str, secret: str) -> (int, int):
             cows += 1
     answer = (bulls, cows)
     return answer
+
+def ask(prompt: str, valid: list[str] = None) -> str:
+    while(True):
+        print(f"{prompt}")
+        inpt = input()
+        if valid is None:
+            break
+        elif inpt in valid:
+            break
+        else:
+            print("Wrong word, use valid words from this:")
+            print(valid)
+    return inpt
+
+def inform(format_string: str, bulls: int, cows: int) -> None:
+    print(format_string.format(bulls, cows))
+
+def gameplay(ask: callable, inform: callable, words: list[str]) -> int:
+    try_cnt = 0
+    secret = random.choice(words)
+    while(True):
+        guess = ask("Введите слово: ", words)
+        try_cnt += 1
+        b, c = bullscows(guess, secret)
+        inform("Быки: {}, Коровы: {}", b, c)
+        if c == 0 and b == len(secret):
+            break
+    return try_cnt
 
 if __name__ == '__main__':
     pass
